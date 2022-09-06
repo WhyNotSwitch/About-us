b7a5c2829277755f443ae03c1d062de0e64d481c
ichristwin <ichristwin@gmail.com>
2022-09-06 19:07:30 +0100

    reponsive

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById("page").onmousemove = function(e) {
 }
 
 document.getElementById("page").ontouchmove = function(e) {
-    document.getElementById("magnify").style.top = e.touches[0].pageY*1 + "px";
-    document.getElementById("magnify").style.left = e.touches[0].pageX*1 + "px";
+    document.getElementById("magnify").style.top = e.touches[0].pageY*1 -50 + "px";
+    document.getElementById("magnify").style.left = e.touches[0].pageX*1 -50 + "px";
 }
