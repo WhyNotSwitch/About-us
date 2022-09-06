3e5f3194e5b50a4b7a3b2614a59f815e44058ba9
ichristwin <ichristwin@gmail.com>
2022-09-06 18:06:21 +0100

    trying out touch

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("page").onmousemove = function(e) {
     document.getElementById("magnify").style.top = e.pageY*1 + "px";
     document.getElementById("magnify").style.left = e.pageX*1 + "px";
 }
+
+document.getElementById("page").ontouchmove = function(e) {
+    document.getElementById("magnify").style.top = e.pageY*1 + "px";
+    document.getElementById("magnify").style.left = e.pageX*1 + "px";
+}
